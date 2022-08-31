ae1505b56b9f33b3ab35d34f80fbc42929b02cb3
danmatuoka <dan_mtk@hotmail.com>
2022-08-31 16:57:31 -0300

    test

--- src/components/SolutionCard/index.tsx
+++ src/components/SolutionCard/index.tsx
@@ -1,28 +1,30 @@
 import { GoThumbsup } from 'react-icons/go';
-import { DivSolutionCard } from './style';
+import { DivPai, DivSolutionCard } from './style';
 
 const SolutionCard = () => (
-  <DivSolutionCard>
-    <div className='card1'>
-      <h3>SOLUÇÃO</h3>
-      <span className='span--tags'>
-        <p>#hashtag</p>
-        <p>#hasttag</p>
-      </span>
-    </div>
-    <div className='card1'>
-      <div className='div--btnlike'>
-        <button className='btn--like' type='button'>
-          <GoThumbsup />
-        </button>
-        <p>10 likes</p>
+  <DivPai>
+    <DivSolutionCard>
+      <div className='card1'>
+        <h3 className='title--card'>SOLUÇÃO</h3>
+        <span className='span--tags'>
+          <p className='hash--card'>#hashtag</p>
+          <p className='hash--card'>#hashtag</p>
+        </span>
       </div>
-      <span className='span--tags'>
-        <p>Comentários</p>
-        <p>Dúvidas</p>
-      </span>
-    </div>
-  </DivSolutionCard>
+      <div className='card1'>
+        <div className='div--btnlike'>
+          <button className='btn--like' type='button'>
+            <GoThumbsup />
+          </button>
+          <p className='hash--card'>10 likes</p>
+        </div>
+        <span className='span--tags'>
+          <p className='hash--card'>Comentários</p>
+          <p className='hash--card'>Dúvidas</p>
+        </span>
+      </div>
+    </DivSolutionCard>
+  </DivPai>
 );
 
 export default SolutionCard;
--- src/components/SolutionCard/style.ts
+++ src/components/SolutionCard/style.ts
@@ -1,18 +1,41 @@
 import styled from 'styled-components';
 
+export const DivPai = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const DivSolutionCard = styled.div`
-  width: 85%;
-  height: 75px;
+  width: 400px;
+  min-height: 75px;
   display: flex;
   justify-content: space-between;
-  margin: 0 auto;
-  border: 1px solid black;
+  /* margin: 0 auto; */
   padding: 0px 10px;
+  border: none;
+  border-left: 5px solid var(--blue);
+  border-right: 5px solid var(--blue);
+  border-radius: 5px;
+  background-color: var(--grey-card);
 
   .card1 {
     display: flex;
     flex-direction: column;
     justify-content: space-around;
+  }
+
+  .card1 .title--card {
+    font-family: ubuntu;
+    color: var(--blue);
+  }
+
+  .card1 .hash--card {
+    font-family: ubuntu;
+    display: flex;
+    align-items: center;
+    font-size: 0.6rem;
   }
 
   .span--tags {
@@ -27,6 +50,7 @@ export const DivSolutionCard = styled.div`
   }
 
   .btn--like {
+    font-size: 1.2rem;
     background-color: transparent;
     border: none;
     color: var(--green);
