258c63c25deca70da0cd8dabfef14e6f2c6a865d
Sofia Alexandrino <sofiaalage@gmail.com>
2022-08-30 21:33:49 -0300

    feat: Button creation

--- src/components/ButtonTag/index.tsx
+++ src/components/ButtonTag/index.tsx
@@ -1,3 +1,27 @@
-const ButtonTag = () => {
-  return <div></div>;
-};
+import React from "react";
+import { Container } from './styles';
+
+
+
+interface ButtonProps {
+  text: React.ReactNode;
+  height: string;
+  onClick: () => void;
+  width: string;
+}
+
+
+const Button = ({text, height, onClick, width}: ButtonProps) => (
+  <Container >
+      <button 
+        type='button' 
+        style={{ height, width }}
+        onClick={onClick}>
+          {text}
+      </button>
+  </Container>
+)
+
+export default Button
+
+
