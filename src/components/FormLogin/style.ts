38827dbc0b0fa905a534aa916e98010bed0d02ba
Sara <saralins19@gmail.com>
2022-09-02 13:18:11 -0300

    feat: adding attributes at the tag Form Login

--- src/components/FormLogin/style.ts
+++ src/components/FormLogin/style.ts
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
+import { FormHTMLAttributes, ReactNode } from 'react';
 
 export const ContainerFormLogin = styled.div`
   width: 50%;
   height: 500px;
 `;
 
-export const FormLogin = styled.form`
+interface IFormProps extends FormHTMLAttributes<HTMLFormElement> {
+  children: ReactNode;
+}
+
+export const FormLogin = styled.form<IFormProps>`
   width: 100%;
   height: 100%;
 `;
