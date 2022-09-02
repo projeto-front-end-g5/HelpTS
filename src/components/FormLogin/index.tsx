9742fc519cb765dae0b598bb311f68c6758499fb
Sara <saralins19@gmail.com>
2022-09-02 13:12:29 -0300

    feat: adding useForm in FormLogin

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -1,26 +1,53 @@
 /* import { Link } from 'react-router-dom'; */
-import { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import { formLoginSchema } from '../../validator/schema';
+import { useLoginContext } from '../../contexts/login/login';
+
 import { ContainerFormLogin, FormLogin as Form } from './style';
 
-const FormLogin = () => {
-  const [openEye, setOpenEye] = useState(false);
+interface IUserLogin {
+  email: string;
+  password: string;
+}
 
-  <ContainerFormLogin>
-    <Form>
-      <label htmlFor='email'>Email </label>
-      <input id='email' type='text' placeholder='Email' />
-      <label htmlFor='password'>Senha</label>
-      <span className='ContainerPassword'>
-        <input id='password' type='password' placeholder='Senha' />
-        <button type='button'>{openEye ? <FaEye /> : <FaEyeSlash />}</button>
-      </span>
-      <button type='submit'>Entrar</button>
-    </Form>
-    <div className='ContainerLinkResgister'>
-      <p>Não possui cadastro? Registre-se aqui!</p>
-    </div>
-  </ContainerFormLogin>;
+const FormLogin = () => {
+  const { openEye, submitLogin } = useLoginContext();
+
+  const { register, handleSubmit } = useForm<IUserLogin>({
+    resolver: yupResolver(formLoginSchema),
+  });
+
+  return (
+    <ContainerFormLogin>
+      <Form onSubmit={handleSubmit(submitLogin)}>
+        <label htmlFor='email'>Email </label>
+        <input
+          id='email'
+          type='text'
+          placeholder='Email'
+          {...register('email')}
+        />
+        <label htmlFor='password'>Senha</label>
+        <span className='ContainerPassword'>
+          <input
+            id='password'
+            type='password'
+            placeholder='Senha'
+            {...register('password')}
+          />
+          <button type='button' onClick={() => submitLogin}>
+            {openEye ? <FaEye /> : <FaEyeSlash />}
+          </button>
+        </span>
+        <button type='submit'>Entrar</button>
+      </Form>
+      <div className='ContainerLinkResgister'>
+        <p>Não possui cadastro? Registre-se aqui!</p>
+      </div>
+    </ContainerFormLogin>
+  );
 };
 
 export default FormLogin;
