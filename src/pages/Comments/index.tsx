6a43b8a9768c78ce0c0a4bbbf78a3285edee97b8
Natália Favaro Cavicchioli <na.cavicchioli@gmail.com>
2022-09-02 09:54:18 -0300

    feat: Card de comentário criado

--- src/pages/Comments/index.tsx
+++ src/pages/Comments/index.tsx
@@ -1,3 +1,12 @@
-const Comments = () => <p>teste comments</p>;
+import CardComment from "../../components/CardComment";
+import CardCommentList from "../../components/CardCommentList";
+import Header from "../../components/Header";
+
+const Comments = () => (
+    <>
+        <Header />
+        <CardCommentList />
+    </>
+)
 
 export default Comments;
