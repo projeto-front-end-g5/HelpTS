851529b5ffe9ac467fced1b3a0a49f9d88509766
danmatuoka <dan_mtk@hotmail.com>
2022-09-04 19:31:17 -0300

    feat: preview solution card e page newpost

---
+++ src/components/PreviewSolutionCard/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const DivTextPreview = styled.div`
+  border: 0.125rem solid var(--blue);
+  padding: 15px;
+  margin-top: 10px;
+  text-align: justify;
+`;
+
+export const DivCodePreview = styled.div`
+  border: 0.125rem solid var(--grey-dark);
+  padding: 15px;
+  margin-top: 10px;
+`;
--- src/pages/CreatePost/index.tsx
+++ src/pages/CreatePost/index.tsx
@@ -1,5 +1,19 @@
+import ContainerBlue from '../../components/ContainerBlue';
 import CreatePostCard from '../../components/CreatePostCard';
+import Header from '../../components/Header';
+import PreviewCardSolution from '../../components/PreviewSolutionCard';
+import { ContainerPreview } from './style';
 
-const CreatePost = () => <CreatePostCard />;
+const CreatePost = () => (
+  <>
+    <Header />
+    <ContainerBlue />
+    <ContainerPreview>
+      <CreatePostCard />
+      <h3>Prévia</h3>
+      <PreviewCardSolution />
+    </ContainerPreview>
+  </>
+);
 
 export default CreatePost;
