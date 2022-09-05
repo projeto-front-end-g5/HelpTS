da6624b40235bd631a3a9a71ec2879367a5ac05e
Natália Favaro Cavicchioli <na.cavicchioli@gmail.com>
2022-09-05 15:04:53 -0300

    feat: SolutionList criada

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,26 +2,24 @@ import DashboardDescription from '../../components/DashboardDescription';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import NavCard from '../../components/NavCard';
-import SolutionCard from '../../components/SolutionCard';
-import { useSolutionsContext } from '../../contexts/solutions/solutions';
+import SolutionList from '../../components/SolutionList';
 import { ButtonAll, DashboardContainer, DivCardDash } from './styles';
 
-const Dashboard = () => {
-  const { solutions } =
-    useSolutionsContext();
-  return <DashboardContainer>
+const Dashboard = () => (
+  <DashboardContainer>
     <Header />
 
     <DashboardDescription />
 
     <section className='main--dashboard'>
       <NavCard />
-      <DivCardDash/>
+      <DivCardDash />
+      <SolutionList />
       <ButtonAll>Mostrar todos</ButtonAll>
     </section>
 
     <Footer targetType='_blank' />
   </DashboardContainer>
-};
+);
 
 export default Dashboard;
