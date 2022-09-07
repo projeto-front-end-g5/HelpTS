151ef5b339eeb065806498654b1bb2294ff005cb
PatrichHAS <you@example.com>
2022-09-07 12:36:47 -0300

    fix: ajustes de funcionalidade

--- src/contexts/user/user.tsx
+++ src/contexts/user/user.tsx
@@ -1,34 +1,32 @@
-import { createContext,useState, useEffect, ReactNode  } from "react";
-
+import { createContext, useState, useEffect, ReactNode } from 'react';
 
 export const UserContext = createContext({});
 
 interface userPoviderProps {
-    children: ReactNode;
-  }
+  children: ReactNode;
+}
 
-
-export const UserProvider = ({ children } : userPoviderProps) => {
-
+export const UserProvider = ({ children }: userPoviderProps) => {
   const [user, setUser] = useState([]);
-  
+
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
 
     if (token != null) {
-      fetch("https://kenziehub.herokuapp.com/profile", {
-        method: "GET",
+      fetch('https://kenziehub.herokuapp.com/profile', {
+        method: 'GET',
         headers: {
           Accept: 'application/json',
-                  'Content-Type': 'application/json',
-                  'Authorization': `Bearer ${token}`
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
         },
       })
         .then((response) => response.json())
         .then((response) => setUser(response))
         .catch((error) => {
-          localStorage.removeItem("token");
-          localStorage.removeItem("userId");
+          console.error(error);
+          localStorage.removeItem('token');
+          localStorage.removeItem('userId');
         });
     }
   }, []);
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,32 +1,37 @@
 import { ImHome } from 'react-icons/im';
+import { useNavigate } from 'react-router-dom';
 import BackgroundTSParticles from '../../components/BackgroundTSParticles';
 import FormLogin from '../../components/FormLogin';
 import LogoRegisterLogin from '../../components/LogoRegisterLogin';
 import { ContainerPageRegister, DivPageRegister } from '../Register/style';
 import { BtnHomeLogoHelpTSContainer } from './style';
 
-const Login = () => (
-  <>
-    <BackgroundTSParticles />
-    <ContainerPageRegister>
-      <DivPageRegister>
-        <BtnHomeLogoHelpTSContainer>
-          <div className='btn--home--container'>
-            <button type='button'>
-              <ImHome />
-            </button>
-          </div>
-          <div className='logo--pageRegister'>
-            <LogoRegisterLogin />
-          </div>
-        </BtnHomeLogoHelpTSContainer>
+const Login = () => {
+  const navigate = useNavigate();
 
-        <div className='container--formRegister'>
-          <FormLogin />
-        </div>
-      </DivPageRegister>
-    </ContainerPageRegister>
-  </>
-);
+  return (
+    <>
+      <BackgroundTSParticles />
+      <ContainerPageRegister>
+        <DivPageRegister>
+          <BtnHomeLogoHelpTSContainer>
+            <div className='btn--home--container'>
+              <button onClick={() => navigate('/dashboard')} type='button'>
+                <ImHome />
+              </button>
+            </div>
+            <div className='logo--pageRegister'>
+              <LogoRegisterLogin />
+            </div>
+          </BtnHomeLogoHelpTSContainer>
+
+          <div className='container--formRegister'>
+            <FormLogin />
+          </div>
+        </DivPageRegister>
+      </ContainerPageRegister>
+    </>
+  );
+};
 
 export default Login;
