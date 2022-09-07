e89ac2d578c2ac9d1c04464d5643646ede828f14
Sara <saralins19@gmail.com>
2022-09-07 08:38:12 -0300

    feat: framer motion

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,24 @@
+import { motion } from 'framer-motion';
 import { ImHome } from 'react-icons/im';
+import { Link } from 'react-router-dom';
 import BackgroundTSParticles from '../../components/BackgroundTSParticles';
 import FormLogin from '../../components/FormLogin';
 import LogoRegisterLogin from '../../components/LogoRegisterLogin';
 import { ContainerPageRegister, DivPageRegister } from '../Register/style';
 
 const Login = () => (
-  <>
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.5 }}
+  >
     <BackgroundTSParticles />
     <ContainerPageRegister>
       <DivPageRegister>
-        <button className='btn--home' type='button'>
+        <Link to='/dashboard' className='btn--home'>
           <ImHome />
-        </button>
+        </Link>
         <div className='logo--pageRegister'>
           <LogoRegisterLogin />
         </div>
@@ -20,7 +27,7 @@ const Login = () => (
         </div>
       </DivPageRegister>
     </ContainerPageRegister>
-  </>
+  </motion.div>
 );
 
 export default Login;
